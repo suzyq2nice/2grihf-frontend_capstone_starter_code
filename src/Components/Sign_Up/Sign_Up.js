2ad9cef9436de937a6e16ac4bbc296a4945b27e2
suzyq2nice <suzyq2nice@gmail.com>
2023-10-06 16:52:41 -0400

    initial message

---
+++ src/Components/Sign_Up/Sign_Up.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Sign_Up.css';
+function SignupForm() {
+  return (
+    <div className="container" style={{ marginTop: '5%' }}>
+      <div className="signup-grid">
+        <div className="signup-text">
+          <h1>Sign Up</h1>
+        </div>
+        <div className="signup-text1" style={{ textAlign: 'left' }}>
+          Already a member? <span><a href="../Login/Login.html" style={{ color: '#2190FF' }}> Login</a></span>
+        </div>
+        <div className="signup-form">
+          <form>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" required className="form-control" placeholder="Enter your name" aria-describedby="helpId" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone</label>
+              <input type="tel" name="phone" id="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required className="form-control" placeholder="Enter your phone number" aria-describedby="helpId" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" required className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" required className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
+            </div>
+            <div className="btn-group">
+              <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Submit</button>
+              <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignupForm;
