e31b51d10907b4c1c1b86ebba91fc67ab3fb8f3d
suzyq2nice <suzyq2nice@gmail.com>
2023-10-09 13:34:51 -0400

    initial message

--- stay-healthy/src/App.js
+++ stay-healthy/src/App.js
@@ -1,22 +1,20 @@
-
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import necessary components
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignUp from './Components/Sign_Up'; 
+import Login from './Components/Login';
+import Navbar from './Components/Navbar'; 
+import LandingPage from './Components/Landing_Page';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Define your Landing Page route */}
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HeroSection />} />
-          {/* Add other routes for the Landing Page components */}
+        <Route path="landing-page" element={<LandingPage />}>
+          <Route path="navbar" element={<Navbar />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="login" element={<Login />} />
         </Route>
-
-        {/* Define other routes for different parts of your application */}
-        {/* For example: */}
-        <Route path="/about" element={<AboutComponent />} />
-        <Route path="/contact" element={<ContactComponent />} />
       </Routes>
     </Router>
   );
